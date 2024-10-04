dc3f3b4dafe1b168416c69a9045357b94d1296ee
andreldgraciano <dedasgdias@gmail.com>
2024-10-04 13:16:54 -0300

    size imgs

--- script.js
+++ script.js
@@ -2,12 +2,9 @@ function contarDias() {
     const dataInicial = new Date("2023-11-26"); // Data inicial
     const dataAtual = new Date(); // Data atual
 
-    // Cálculo da diferença em milissegundos
     const diferencaTempo = dataAtual - dataInicial;
 
-    // Converter de milissegundos para dias
     const diasPassados = Math.floor(diferencaTempo / (1000 * 60 * 60 * 24));
 
-    // Inserir o valor no h1 com id "days"
     document.getElementById("days").textContent = diasPassados + ' dias';
 }
