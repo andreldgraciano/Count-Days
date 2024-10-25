182d11f65a8e42d9cc740302abf39870ac9e02bc
andreldgraciano <dedasgdias@gmail.com>
2024-10-25 11:40:55 -0300

    add cat img

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,33 @@ coracao.addEventListener('click', function () {
         audioPlayer.pause();
     }
 });
+
+function atualizarImagem() {
+    const imgGato = document.getElementById('imgGato');
+
+    // Esconde a imagem enquanto a nova imagem é carregada
+    
+
+    fetch("https://api.thecatapi.com/v1/images/search?api_key=live_5xYND8fOJuz5vPIl4tJKN05ABZbtWEWd035ehl7ufAczofRwawk3ktMvou1zIUmW")
+        .then(response => response.json())
+        .then(data => {
+            const imageUrl = data[0].url;
+
+            // Define o evento onload para mostrar a imagem quando ela for carregada
+            imgGato.onload = function() {
+                imgGato.classList.remove('hidden');  // Exibe a imagem quando carregada
+            };
+
+            // Atualiza o src da imagem para a nova URL
+            imgGato.src = imageUrl;
+        })
+        .catch(error => {
+            console.error("Erro ao buscar a imagem:", error);
+        });
+}
+
+// Chama a função a cada 5 segundos
+setInterval(atualizarImagem, 5000);
+
+// Chama imediatamente a primeira vez para carregar uma imagem logo que a página é carregada
+atualizarImagem();
